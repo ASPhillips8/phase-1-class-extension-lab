2fb9b32d14bb7e4f111e476240d19c7198dfe6bd
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-05 23:35:59 -0400

    Create Polygon class

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+//sides = [side, side, side, side]
+class Polygon {
+  constructor(sides) {
+    this.sides = sides
+  }
+
+  get countSides() {
+    return this.sides.length
+  }
+
+  get perimeter() {
+    return this.sides.reduce((acc, side) => acc + side, 0)
+    }
+
+}
+
+
