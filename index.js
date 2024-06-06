ab98298ec5e190f5fa82e3b26ff94c0ee24bac99
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-06 09:35:01 -0400

    Create isValid method

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ class Polygon {
   get perimeter() {
     return this.sides.reduce((acc, side) => acc + side, 0)
     }
-
 }
 
+class Triangle extends Polygon {
+  get isValid() {
+   const [sideA, sideB, sideC] = this.sides
+
+    return sideA+sideB > sideC && sideA+sideC > sideB && sideB+sideC > sideA
+  }
+
+}
+/// need to get the side
+/// a+b > c
+/// a+c > b
+/// b+c > a
+
+let triangle = new Triangle ( [ 5, 5, 5 ] )
+let triangle2 = new Triangle( [ 15, 10, 1 ] )
+
+
+
+console.log(triangle.countSides)
+console.log(triangle.perimeter)
+
+console.log(triangle2.countSides)
+console.log(triangle.perimeter)
 
